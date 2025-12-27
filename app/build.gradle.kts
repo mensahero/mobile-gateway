37f4e1c594d1fc817cd7701827529b7dd73b1042
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-12-27 11:47:01 +0800

    feat: Add Kwik UI library and enable desugaring

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlin {
@@ -53,6 +54,9 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
 
+    //UI
+    implementation("com.isakaro:kwik.ui:1.0.14")
+
     // MVVM
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.10.0")
     // Coroutines + StateFlow
@@ -71,4 +75,5 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
 }
